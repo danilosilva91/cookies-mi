af4acc930b42ada90467597d80825c82117204ea
Danilo Silva <226077658+danilosilva91@users.noreply.github.com>
2025-10-30 22:09:39 -0300

    aprendendo js

---
+++ script.js
@@ -0,0 +1,19 @@
+//getElementById
+
+const abacate = document.getElementById("inicio");
+console.log(abacate);
+
+//getElementsByClassName
+
+const banana = document.getElementsByClassName("prints-redes-sociais");
+console.log(banana);
+
+//getElementsByTagName
+
+const cereja = document.getElementsByTagName("p");
+console.log(cereja);
+
+//getElementsByName
+
+const damasco = document.getElementsByName("nome-completo");
+console.log(damasco);

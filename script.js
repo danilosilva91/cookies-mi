ab3ad88029fe7ac90c9b8167275e03bd163d6e0f
Danilo Silva <226077658+danilosilva91@users.noreply.github.com>
2025-11-04 20:33:11 -0300

    aprendendo ferramentas js

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ console.log(cereja);
 
 const damasco = document.getElementsByName("nome-completo");
 console.log(damasco);
+
+//querySelector -> seleciona o primeiro elemento que encontrar com o seletor CSS
+
+const emma = document.querySelector(".contato-item");
+console.log(emma);
+
+
+//querySelectorAll -> seleciona todos os elementos que encontrarem com o seletor CSS
+
+const figo = document.querySelectorAll(".contato-item");
+console.log(figo);
+
+const element = document.querySelector("p");
+
+console.log(element.textContent); //só html
+console.log(element.innerText); //html e css
+console.log(element.innerHTML); //traz tudo 
+
+const button = document.querySelector("button");
+
+button.style.backgroundColor = "purple";
+
+const input = document.querySelector("input");
+
+function cliqueiNoBotao() {
+    paragrafo.textContent = (input.value);
+}
+
+function digiteiNoInput() {
+    console.log(input.value);
+}
+
+const paragrafo = document.querySelector(".paragrafo");
